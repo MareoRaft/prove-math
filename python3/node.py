c6df5ba7926f5a1f3915265ee0a01a5d17832ee8
theodoresiu <theodoresiu7@gmail.com>
2015-06-04 13:50:17 -0400

    node.py updates

--- python3/node.py
+++ python3/node.py
@@ -6,35 +6,71 @@ from warnings import warn
 
 class node:
 
-	def __init__(self, dictionary):
-		for key in dictionary:
-			if key is in keys of translation hash:
-				translate it to the value
-			if key is 'definition':
-				self.definition = dictionary[key]
-			else if key is 'theorem':
-				j
-			else if ...
-			else
-				warn('Unidentified key.')
+	# Pass in a single json doc in order to convert to a node
+	def __init__(self, doc):
+		self.name=doc["name"]
+		self.type=doc["type"]
+		self.weight=doc["weight"]
+		self.description=doc["description"]
+		self.intuition=[]
+		self.examples=[]
+		self.notes=[]
+		if "intuition" in doc:
+			self.intuition=doc["intuition"]
+		if "examples" in doc:
+			for single_examples in doc["examples"]:
+				self.examples.append(single_examples)
+				
+		if "notes" in doc:
+			for single_notes in doc["notes"]:
+				self.notes.append(single_notes)
 
-
+	def __repr__(self):
+		
+		msg="(%s,%s,%s,%d)\n" %(self.name,self.type,self.description,self.weight)
+		if self.intuition:
+			msg=msg+self.intuition+"\n"
+		for example in self.examples:
+			msg=msg+example+"\n"
+		
+		return msg
+"""
 	@property
 	def type(self):
-		return self.__type
+		return self.type
 
 	@type.setter
 	def type(self, type):
 		if type is ('definition' or 'theorem' or 'exercise'):
-			self.__type = type
+			self.type = type
 		else:
 			warn('Bad type.')
-
-
 	@property
 	def definition(self):
 		return self.__definition
+"""
+if __name__=="__main__":
 
+	print("Hello World")
+	sample_theorem={"name":"Pythagorean theorem","type":"theorem","weight":1,"description":"a^2+b^2=c^2","intuition":"A simple explanation","examples":["Example 1","Example 2"]}
+	sample_definition={"name":"triangle","type":"definition","weight":1,"description":"3 sided polygon","intuition":"A simple explanation","examples":["Example 1","Example 2"]}
+	a=node(sample_theorem)
+	b=node(sample_definition)
+	print(a)
+	print(b)
+
+
+		
+
+
+
+
+
+
+
+
+
+"""
 	@definition.setter
 	def definition(self, definition):
 		if not definition has underlined word:
@@ -59,4 +95,4 @@ class node:
 	# def examples # array of examples
 
 	# def counter_examples
-
+"""
