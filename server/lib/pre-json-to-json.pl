37b4a70d3ebdc0b07736d1e3850968c88679b876
Matthew Lancellotti <mareoraft@gmail.com>
2015-06-24 16:50:20 -0400

    more content

--- server/lib/pre-json-to-json.pl
+++ server/lib/pre-json-to-json.pl
@@ -2,6 +2,31 @@
 
 # This perl script will use regex to convert .pre-json files to .json files
 
+# regex to recognize a json value:
+# see json.org !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+my $e = qr([eE][+-]?);
+# $digit is \d, so we'll just type \d
+# $digits is \d+, so we'll just type \d+
+my $exp = qr($e\d+);
+my $frac = qr(\.\d+);
+my $int = qr(-?\d(?:[1-9]\d+)?);
+my $number = qr($int(?:$frac|$exp|$frac$exp));
+
+my $char = qr([^"\\]|\\(?:["\\/bfnrt]|uFOURHEXDIGITS));
+my $string = qr("$char*");
+
+my $value = qr($string|$number|$OBJECT|$ARRAY|true|false|null);
+
+my $elements = qr($value(?:\s*,\s*$value)*);
+my $array = qr(\[\s*$elements?\s*\]);
+
+my $pair = qr($string\s*:\s*$value);
+
+my $members = qr($pair(?:\s*,\s*$pair)*);
+my $object = qr(\{\s*$members?\s*\});
+
+
+if( '0.9i' =~ $number ){ print 'success' }
 
 # 1. All comments will be deleted
 
