af65ab13e78daf5a602524168c5b57e7eae50e2e
Matthew Lancellotti <mareoraft@gmail.com>
2015-06-25 23:35:05 -0400

    unit test correctly formatted!  still needs the rest of the tests

--- server/lib/test_JSONRegex.pl
+++ server/lib/test_JSONRegex.pl
@@ -6,7 +6,7 @@ use Test::More;
 sub ok_all_match{
 	my ($arrayref, $regex) = @_;
 	for my $el (@$arrayref) {
-		ok( $el =~ $regex );
+		ok( $el =~ $regex ) or warn "Failed element: $el";
 	}
 	return 1
 }
@@ -14,7 +14,7 @@ sub ok_all_match{
 sub ok_all_mismatch{
 	my ($arrayref, $regex) = @_;
 	for my $el (@$arrayref) {
-		ok( $el !~ $regex );
+		ok( $el !~ $regex ) or warn "Failed element: $el";
 	}
 	return 1
 }
@@ -25,10 +25,10 @@ my @fracs = qw(.0 .00 .01 .10 .876 .03247650803824756083476508237465087326450873
 my @nonfracs = qw(. 0 7 -8);
 	ok_all_mismatch( \@nonfracs, qr/^$JSONRegex::frac$/ );
 
-my @natural_numbers = qw(0 1 100000 01 1932847 1034972560823765087365087348736083247);
-	ok_all_match( \@natural_numbers, qr/^$JSONRegex::natural_numbers$/ );
-# my @nonnatural_numbers = (qw(-135 -1 -0), @fracs);
-# 	ok_all_mismatch( \@nonnatural_numbers, qr/^$JSONRegex::natural_numbers$/ );
+my @natural_numbers = qw(0 1 100000 1932847 1034972560823765087365087348736083247);
+	ok_all_match( \@natural_numbers, qr/^$JSONRegex::natural_number$/ );
+my @nonnatural_numbers = (qw(-135 -1 -0 01), @fracs);
+	ok_all_mismatch( \@nonnatural_numbers, qr/^$JSONRegex::natural_number$/ );
 
 
 done_testing;
