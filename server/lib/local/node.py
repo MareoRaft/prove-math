27425acc7e40c4901cb4b925792fdb773d0b5044
theodoresiu <theodoresiu7@gmail.com>
2015-06-29 15:55:45 -0400

    Regex for classes roughly done

--- server/lib/local/node.py
+++ server/lib/local/node.py
@@ -42,29 +42,128 @@ class Node:
 
 	# Pass in a single json dictionary (dic) in order to convert to a node
 	def __init__(self, dic):
-		self.name = dic["name"] # Thanks to the @name.setter, we can (and should) use self.name = value syntax.
-		self.type = dic["type"]
-		self.weight = dic["weight"]
-		self.description = dic["description"]
+
+					
 		self.intuition = []
 		self.examples = []
 		self.notes = []
-		if "intuition" in dic:
-			self.intuition = dic["intuition"]
-		if "examples" in dic:
-			for single_examples in dic["examples"]:
-				self.examples.append(single_examples)
+		self.proofs=[]
+		self.plural=None
 
-		if "notes" in dic:
-			for single_notes in dic["notes"]:
-				self.notes.append(single_notes)
+		for key in dic.keys():
+			if re.match(r'name.*',key, re.IGNORECASE):
+				self.name = dic[key]
+			
+			elif re.match(r'weight.*',key,re.IGNORECASE):
+				self.weight = dic[key]
+			
+			elif re.match(r'th.*',key,re.IGNORECASE):
+				self.type="theorem"
+				self.description=dic[key]
+				for ii in dic.keys():
+					if re.match(r'proof.*',ii,re.IGNORECASE):
+						if isinstance(dic[ii],dict):
+							self.proofs.append(dic[ii])
+						else:
+							for x in dic[ii]:
+								if isinstance(x,dict):
+									self.proofs.append(x)
+			
+			elif re.match(r'def.*',key,re.IGNORECASE):
+				self.type="definition"
+				self.description=dic[key]
+				if re.search(r'__.*__',dic[key]):
+					word=re.search(r'__.*__',dic[key]).group()
+					print(word)
+					self.name=word.strip("__")
+				for ii in dic.keys():	
+					if re.match(r'pl',ii,re.IGNORECASE):
+						self.plural=dic[ii]
+			
+			elif re.match(r'type.*',key,re.IGNORECASE) and re.match(r'def.*',dic[key],re.IGNORECASE):
+				self.type=dic[key]
+				for ii in dic.keys():	
+					if re.match(r'pl',ii,re.IGNORECASE):
+						self.plural=dic[ii]
+			
+			elif re.match(r'type.*',key,re.IGNORECASE) and re.match(r'th.*',dic[key],re.IGNORECASE):
+				self.type=dic[key]
+				for ii in dic.keys():
+					if re.match(r'proof.*',ii,re.IGNORECASE):
+						if isinstance(dic[ii],dict):
+							self.proofs.append(dic[ii])
+						else:
+							for x in dic[ii]:
+								self.proofs.append(x)
+			
+
+			elif re.match(r'des.*',key,re.IGNORECASE):
+				self.description=dic[key]
+			
+			elif re.match(r'intuit.*',key,re.IGNORECASE):
+				if isinstance(dic[key],list):
+					for single_intuition in dic[key]:
+						if isinstance(single_intuition,str):
+							self.intuition.append(single_intuition)
+				else:
+					self.intuition = dic[key]
+			
+			elif re.match(r'example.*',key,re.IGNORECASE):
+				if isinstance(dic[key],list):
+					for single_examples in dic[key]:
+						if isinstance(single_examples,str):
+							self.examples.append(single_examples)
+				else:
+					self.examples.append(dic[key])
+
+			elif re.match(r'note.*',key,re.IGNORECASE):
+				if isinstance(dic[key],list):
+					for single_notes in dic[key]:
+						if isinstance(single_notes,str):
+							self.notes.append(single_notes)
+				else:
+					self.notes.append(dic[key])
+			
+			#At this point python will begin guessing
+			elif  isinstance(dic[key], str) and  re.search(r'__.*__',dic[key]):
+				self.type="definition"
+				self.description=dic[key]
+				word=re.search(r'__.*__',dic[key]).group()
+				print(word)
+				self.name=word.strip("__")
+
+			elif isinstance(dic[key], str) and re.search(r'\$',dic[key]):
+				self.type="theorem"
+				self.description=dic[key]
+
+				
+
+				
+
 
 	def __repr__(self):
-		msg = "(%s,%s,%s,%d)\n" % (self.name,self.type,self.description,self._weight)
-		if self._intuition:
-			msg = msg + self._intuition + "\n"
-		for example in self._examples:
-			msg = msg + example + "\n"
+		if self.type=="definition":
+			msg = "(%s,%s,%s,%s,%d)\n" % (self.name, self.plural, self.type, self.description, self.weight)
+			if self._intuition:
+				msg = msg + self._intuition + "\n"
+			for example in self._examples:
+				msg = msg + example + "\n"
+			for single_note in self._notes:
+				msg=msg+ single_note+"\n"
+		
+		else:
+			msg = "(%s,%s,%s,%d)\n" % (self.name,self.type,self.description,self.weight)
+			if self._intuition:
+				msg = msg + self._intuition + "\n"
+			for example in self._examples:
+				msg = msg + example + "\n"
+			for single_note in self._notes:
+				msg=msg+ single_note+"\n"
+			for single_proof in self._proofs:
+				for x in single_proof:
+					msg=msg+str(x)+": "+single_proof[x]+"\n"
+
+		
 		return msg
 
 	def __eq__(self,other):
@@ -108,7 +207,7 @@ class Node:
 		elif re.match(r'th.*', new_type):
 			self._type = 'theorem'
 		else:
-			warn('Bad type.')
+			warn('Bad type. Must either be a theorem or definition')
 
 	@property
 	def weight(self):
@@ -125,7 +224,7 @@ class Node:
 	def description(self):
 		return self._description
 
-	@weight.setter
+	@description.setter
 	def description(self, new_description):
 		self._description = new_description
 
@@ -152,6 +251,23 @@ class Node:
 	@notes.setter
 	def notes(self, new_notes):
 		self._notes = new_notes
+	@property
+	def plural(self):
+		return self._plural
+	@plural.setter
+	def plural(self, new_plural):
+		self._plural=new_plural
+
+	@property
+	def proofs(self):
+		return self._proofs
+	
+	@proofs.setter
+	def proofs(self,new_proofs):
+		self._proofs=new_proofs
+	
+	def append_proof(self, add_proof):
+		self._proofs.append(add_proof)
 
 	def append_intuition(self, add_intuition):
 		self._intuition.append(add_intuition)
@@ -170,16 +286,55 @@ class Node:
 if __name__=="__main__":
 
 	sample_theorem = {"name": "Pythagorean theorem", "type": "theorem", "weight": 1, "description": "a^2+b^2=c^2", "intuition": "A simple explanation", "examples": ["Example 1", "Example 2"]}
-	sample_definition = {"name": "triangle", "type": "definition", "weight": 1, "description": "3 sided polygon", "intuition": "A simple explanation", "examples": ["Example 1", "Example 2"]}
+	sample_definition = {"name": "triangle","plural":"triangles", "type": "definition", "weight": 1, "description": "3 sided polygon", "intuition": "A simple explanation", "examples": ["Example 1", "Example 2"]}
+	matt_example1=	{
+			"name": "Choosy Identity",
+			"weight": 3,
+			"thm": "$ \\binom nk = \\binom{n}{n-k} $",
+			"proofs": [
+				"Apply $\\binom nk = \\frac{n!}{k!(n-k)!} = \\frac{n!}{[n-(n-k)]!(n-k)!} = \\binom{n}{n-k} $",
+				{
+					"type": "combinatorial",
+					"content": "Left side: We have $n$ puppies.  We choose $k$ of them and color them blue.  We color the rest of them red.  Right side: We have $n$ puppies.  We choose $n-k$ of them and color them red.  We color the rest of them blue.",
+				},
+			],
+		}
+
+	matt_example2=	{
+			"weight": 4,
+			"type": "exercise",
+			"content": "$ k \\binom nk = n \\binom{n-1}{k-1} $",
+			"proof": {
+				"type": "combinatorial",
+				"content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee.",
+			},
+		}
+
+	thomas_example=	{"definition":"If $E \subset S$, and $\exists \gamma \in S$ such that $\forall x \in E, x \geq \gamma$, then we say that $E$ is __bounded below__ by $\gamma"}
+
+
+
 	a = Node(sample_theorem)
 	b = Node(sample_definition)
+	c= Node(matt_example1)
+	d=Node(matt_example2)
+	e=Node(thomas_example)
 	print(a)
 	print(b)
-	#Importing the same documents from a file
+	print(c)
+	print(d.__dict__)
+	print(e.__dict__)
+        #Importing the same documents from a file
+	
 	data_dictionary = helper.json_import('../../data/data-test.json')
 	for x in data_dictionary['nodes']:
 		c = Node(x)
 		print(c)
+
+	new_data_dictionary = helper.json_import('../../data/commas-removed.json')
+	for x in new_data_dictionary['nodes']:
+		c=Node(x)
+		print(c.__dict__)
 
 
 
