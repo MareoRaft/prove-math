57cbd2fc08db67a6c4d87f955b3d593ad89297d5
Matthew Lancellotti <mareoraft@gmail.com>
2015-07-03 18:48:21 -0400

    began restructuring node

--- server/lib/local/node.py
+++ server/lib/local/node.py
@@ -3,20 +3,15 @@
 # standard library:
 #Reg expressions for processing definition, name, etc..
 #Stricter Reg Expressions with failure at else
-#Mongo class 
 import sys
 from warnings import warn
 import subprocess
 import copy
 import re
 
-
 # local:
-#It appears that helper has been moved to the parent directory
-sys.path.insert(0, '..')
 import helper
 from mongo import Mongo
-
 
 ################################### HELPERS ###################################
 # Should we make mongo variables global for all methods?
@@ -26,13 +21,11 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 4:
 def to_bash():
 	# include commands here to be executed in bash
 	bash_out = subprocess.check_output('ls; cd; ls', shell=True)
-	print (bash_out)
+	print(bash_out)
 	subprocess.call('mkdir test_folder', shell=True)
 
 
 #################################### MAIN #####################################
-# I would like to make a python3 script which uses this class and allows me to manually input new definitions, theorems, lemmas, etc.
-# def vs definition, make clone a method, node is capitalized
 # MathJax/Node.js, run bash commands from python script, export json
 # update __repr__ and __str__ to follow Python standard
 # To Do: Equality method, magic methods, people o-auth, login info
@@ -43,37 +36,37 @@ class Node:
 
 	# Pass in a single json dictionary (dic) in order to convert to a node
 	def __init__(self, dic):
-
-					
 		self.intuition = []
 		self.examples = []
 		self.notes = []
-		self.proofs=[]
-		self.plural=None
+		self.proofs = []
+		self.plural = None
 
 		for key in dic.keys():
-			if re.match(r'name.*',key, re.IGNORECASE):
+			if re.match(r'name$', key, re.IGNORECASE):
 				self.name = dic[key]
-			
-			elif re.match(r'weight.*',key,re.IGNORECASE):
+
+			# re.match starts at the beginning of the string by default.
+			# so re.match('hi$') is equivalent to re.search('^hi$')
+			elif re.match(r'weight$', key, re.IGNORECASE):
 				self.weight = dic[key]
-			
-			elif re.match(r'^(theorem|thm)$',key,re.IGNORECASE):
-				self.type="theorem"
-				self.description=dic[key]
-				for ii in dic.keys():
-					if re.match(r'proof.*',ii,re.IGNORECASE):
-						if isinstance(dic[ii],dict):
+
+			elif re.match(r'(?:theorem|thm)$', key, re.IGNORECASE):
+				self.type = "theorem"
+				self.description = dic[key]
+				for ii in dic.keys(): # this needs to be restructured as we write things as subclasses
+					if re.match(r'proofs?$', ii, re.IGNORECASE):
+						if isinstance(dic[ii], dict):
 							self.proofs.append(dic[ii])
 						else:
 							for x in dic[ii]:
 								if isinstance(x,dict):
 									self.proofs.append(x)
-			
-			elif re.match(r'def.*',key,re.IGNORECASE):
-				self.type="definition"
-				self.description=dic[key]
-				if re.findall(r'__[^_]*__',dic[key]):
+
+			elif re.match(r'(?:def|defn|definition)$', key, re.IGNORECASE):
+				self.type = "definition"
+				self.description = dic[key]
+				if re.findall(r'__[^_]*__',dic[key]): # if no dunderscores are found, we should fail with an error message
 					word=re.findall(r'__[^_]*__',dic[key])
 					if len(word)==1:
 						self.name=word[0].strip("__")
@@ -83,16 +76,16 @@ class Node:
 							compound_name=compound_name+x.strip("__")+"/"
 						self.name=compound_name.strip("/")
 
-				for ii in dic.keys():	
+				for ii in dic.keys():
 					if re.match(r'pl',ii,re.IGNORECASE):
 						self.plural=dic[ii]
-			
+
 			elif re.match(r'type.*',key,re.IGNORECASE) and re.match(r'def.*',dic[key],re.IGNORECASE):
 				self.type=dic[key]
-				for ii in dic.keys():	
+				for ii in dic.keys():
 					if re.match(r'pl',ii,re.IGNORECASE):
 						self.plural=dic[ii]
-			
+
 			elif re.match(r'type.*',key,re.IGNORECASE) and re.match(r'th.*',dic[key],re.IGNORECASE):
 				self.type=dic[key]
 				for ii in dic.keys():
@@ -102,19 +95,19 @@ class Node:
 						else:
 							for x in dic[ii]:
 								self.proofs.append(x)
-			
 
-			elif re.match(r'des.*',key,re.IGNORECASE) or re.match(r'content.*',key, re.IGNORECASE):
+
+			elif re.match(r'des.*|contentmmmm',key,re.IGNORECASE) or re.match(r'content.*',key, re.IGNORECASE): # this needs to accept "content" as a synonym for description
 				self.description=dic[key]
-			
-			elif re.match(r'intuit.*',key,re.IGNORECASE):
+
+			elif re.match(r'intuit.*',key,re.IGNORECASE): # a lot of these seem like the same code, but one with "intuition", one with "example", one with "note", etc.  Try writing a function which takes in "intuition", etc, and does this so that you don't have to repeat the code.
 				if isinstance(dic[key],list):
 					for single_intuition in dic[key]:
 						if isinstance(single_intuition,str):
 							self.intuition.append(single_intuition)
 				else:
 					self.intuition = dic[key]
-			
+
 			elif re.match(r'example.*',key,re.IGNORECASE):
 				if isinstance(dic[key],list):
 					for single_examples in dic[key]:
@@ -130,28 +123,11 @@ class Node:
 							self.notes.append(single_notes)
 				else:
 					self.notes.append(dic[key])
-			
-			#At this point python will begin guessing
-			elif  isinstance(dic[key], str) and  re.search(r'__[^_]*__',dic[key]):
-				self.type="definition"
-				self.description=dic[key]
-				word=re.findall(r'__[^_]*__',dic[key]).group()
-				if len(word)==1:
-					self.name=word.strip("__")
-				else:
-					compound_name=""
-					for x in word:
-						compound_name=compound_name+x.strip("__")+"/"
-					self.name=compound_name.strip("/")
+
+			else: output an error. key is not a valid key name.
 
 
-			elif isinstance(dic[key], str) and re.search(r'\$',dic[key]):
-				self.type="theorem"
-				self.description=dic[key]
 
-				
-
-				
 
 
 	def __repr__(self):
@@ -163,7 +139,7 @@ class Node:
 				msg = msg + example + "\n"
 			for single_note in self._notes:
 				msg=msg+ single_note+"\n"
-		
+
 		else:
 			msg = "(%s,%s,%s,%d)\n" % (self.name,self.type,self.description,self.weight)
 			if self._intuition:
@@ -176,7 +152,6 @@ class Node:
 				for x in single_proof:
 					msg=msg+str(x)+": "+single_proof[x]+"\n"
 
-		
 		return msg
 
 	def __eq__(self,other):
@@ -198,8 +173,8 @@ class Node:
 			if x not in other.notes:
 				return False
 		return True
-	
-		
+
+
 
 	@property
 	def name(self):
@@ -274,11 +249,11 @@ class Node:
 	@property
 	def proofs(self):
 		return self._proofs
-	
+
 	@proofs.setter
 	def proofs(self,new_proofs):
 		self._proofs=new_proofs
-	
+
 	def append_proof(self, add_proof):
 		self._proofs.append(add_proof)
 
@@ -296,68 +271,9 @@ class Node:
 		return clone
 
 
-if __name__=="__main__":
-
-	sample_theorem = {"name": "Pythagorean theorem", "type": "theorem", "weight": 1, "description": "a^2+b^2=c^2", "intuition": "A simple explanation", "examples": ["Example 1", "Example 2"]}
-	sample_definition = {"name": "triangle","plural":"triangles", "type": "definition", "weight": 1, "description": "3 sided polygon", "intuition": "A simple explanation", "examples": ["Example 1", "Example 2"]}
-	matt_example1=	{
-			"name": "Choosy Identity",
-			"weight": 3,
-			"thm": "$ \\binom nk = \\binom{n}{n-k} $",
-			"proofs": [
-				"Apply $\\binom nk = \\frac{n!}{k!(n-k)!} = \\frac{n!}{[n-(n-k)]!(n-k)!} = \\binom{n}{n-k} $",
-				{
-					"type": "combinatorial",
-					"content": "Left side: We have $n$ puppies.  We choose $k$ of them and color them blue.  We color the rest of them red.  Right side: We have $n$ puppies.  We choose $n-k$ of them and color them red.  We color the rest of them blue.",
-				},
-			],
-		}
-
-	matt_example2=	{
-			"weight": 4,
-			"type": "exercise",
-			"content": "$ k \\binom nk = n \\binom{n-1}{k-1} $",
-			"proof": {
-				"type": "combinatorial",
-				"content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee.",
-			},
-		}
-
-	thomas_example=	{"definition":"If $E \subset S$, and $\exists \gamma \in S$ such that $\forall x \in E, x \geq \gamma$, then we say that $E$ is __bounded below__ by $\gamma"}
-
-	thomas_example_1={"definition":"If $E$ is __bounded gamma__ by $\gamma$, then we say that $\gamma$ is a __lower bound__ of $E$"}
 
 
-
-	#a = Node(sample_theorem)
-	#b = Node(sample_definition)
-	#c= Node(matt_example1)
-	#d=Node(matt_example2)
-	#e=Node(thomas_example)
-	#f=Node(thomas_example_1)
-	#print(a)
-	#print(b)
-	#print(c)
-	#print(d.__dict__)
-	#print(e.__dict__)
-	#print(f.__dict__)
-        #Importing the same documents from a file
-	
-	#data_dictionary = helper.json_import('../../data/data-test.json')
-	#for x in data_dictionary['nodes']:
-	#	c = Node(x)
-		#print(c)
-	a=Mongo("provemath", "combinatorics")
-
-	new_data_dictionary = helper.json_import('../../data/combinatorics.json')
-	for x in new_data_dictionary['nodes']:
-		c=Node(x)
-		#print(c.__dict__)
-		a.single_insert_to_mongo(c.__dict__)
-
-
-
-
+# we should implement this stuff below and then delete the string:
 """
 	@definition.setter
 	def definition(self, definition):
--- server/lib/local/test_node.py
+++ server/lib/local/test_node.py
@@ -17,3 +17,63 @@ def test_node_copy():
     assert a==b
     pass
 
+def test_Theorem___init__():
+    sample_theorem = {"name": "Pythagorean theorem", "type": "theorem", "weight": 1, "description": "a^2+b^2=c^2", "intuition": "A simple explanation", "examples": ["Example 1", "Example 2"]}
+
+def test_stuff():
+    sample_definition = {"name": "triangle","plural":"triangles", "type": "definition", "weight": 1, "description": "3 sided polygon", "intuition": "A simple explanation", "examples": ["Example 1", "Example 2"]}
+    matt_example1=  {
+            "name": "Choosy Identity",
+            "weight": 3,
+            "thm": "$ \\binom nk = \\binom{n}{n-k} $",
+            "proofs": [
+                "Apply $\\binom nk = \\frac{n!}{k!(n-k)!} = \\frac{n!}{[n-(n-k)]!(n-k)!} = \\binom{n}{n-k} $",
+                {
+                    "type": "combinatorial",
+                    "content": "Left side: We have $n$ puppies.  We choose $k$ of them and color them blue.  We color the rest of them red.  Right side: We have $n$ puppies.  We choose $n-k$ of them and color them red.  We color the rest of them blue.",
+                },
+            ],
+        }
+
+    matt_example2=  {
+            "weight": 4,
+            "type": "exercise",
+            "content": "$ k \\binom nk = n \\binom{n-1}{k-1} $",
+            "proof": {
+                "type": "combinatorial",
+                "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee.",
+            },
+        }
+
+    thomas_example= {"definition":"If $E \subset S$, and $\exists \gamma \in S$ such that $\forall x \in E, x \geq \gamma$, then we say that $E$ is __bounded below__ by $\gamma"}
+
+    thomas_example_1={"definition":"If $E$ is __bounded gamma__ by $\gamma$, then we say that $\gamma$ is a __lower bound__ of $E$"}
+
+
+
+    #a = Node(sample_theorem) # incorporate this commented stuff into a test or delete it
+    #b = Node(sample_definition)
+    #c= Node(matt_example1)
+    #d=Node(matt_example2)
+    #e=Node(thomas_example)
+    #f=Node(thomas_example_1)
+    #print(a)
+    #print(b)
+    #print(c)
+    #print(d.__dict__)
+    #print(e.__dict__)
+    #print(f.__dict__)
+        #Importing the same documents from a file
+
+    #data_dictionary = helper.json_import('../../data/data-test.json')
+    #for x in data_dictionary['nodes']:
+    #   c = Node(x)
+        #print(c)
+    a=Mongo("provemath", "combinatorics")
+
+    new_data_dictionary = helper.json_import('../../data/combinatorics.json')
+    for x in new_data_dictionary['nodes']:
+        c=Node(x)
+        #print(c.__dict__)
+        a.single_insert_to_mongo(c.__dict__)
+
