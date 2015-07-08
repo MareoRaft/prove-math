ae4b8660d8c80289862358012c33b9621aee8d7b
theodoresiu <theodoresiu7@gmail.com>
2015-07-08 11:59:00 -0400

    Small fix

--- server/lib/node.py
+++ server/lib/node.py
@@ -111,16 +111,18 @@ class Node:
 	def __str__(self):
 		if self.type=="definition":
 			msg = "(%s,%s,%s,%s,%d)\n" % (self.name, self.plural, self.type, self.description, self.weight)
-			#if self._intuition:
-			#	msg = msg + self._intuition + "\n"
+			if self._intuitions:
+				for intuition in self._intuitions:
+					msg = msg + self._intuitions + "\n"
 			for example in self._examples:
 				msg = msg + example + "\n"
 			#for single_note in self._notes:
 			#	msg=msg+ single_note+"\n"
 		else:
 			msg = "(%s,%s,%s,%d)\n" % (self.name,self.type,self.description,self.weight)
-			#if self._intuition:
-			#	msg = msg + self._intuition + "\n"
+			if self._intuitions:
+				for intuition in self._intuitions:
+					msg = msg + intuition + "\n"
 			for example in self._examples:
 				msg = msg + example + "\n"
 			#for single_note in self._notes:
