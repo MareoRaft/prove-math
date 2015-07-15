70514d8c6b034fe9d8c1c2063ea0c6434b4fba07
theodoresiu <theodoresiu7@gmail.com>
2015-07-15 11:27:04 -0400

    node and test_node updated

--- server/lib/node.py
+++ server/lib/node.py
@@ -1,4 +1,4 @@
-#7/14 Need to fix dunderscore issue, printing of nodes,
+#7/14 Need to fix dunderscore issue
 ##################################### TODO ####################################
 #Reg expressions for processing definition, name, etc..
 #Stricter Reg Expressions with failure at else
@@ -78,6 +78,7 @@ def dunderscore_count(string):
 
 
 
+
 #################################### MAIN #####################################
 
 
@@ -96,7 +97,7 @@ class Node:
 		self.weight = move_attribute(dic, {'weight'})
 		self.intuitions = move_attribute(dic, {'intuitions', 'intuition'}, strict=False)
 		self.examples = move_attribute(dic, {'examples', 'example'}, strict=False)
-		self.counterexamples=move_attribute(dic,{'counterexample','counterexamples'},strict=False)
+		self.counterexamples=move_attribute(dic,{'counterexample','counterexamples','counter_example','counter_examples'},strict=False)
 		self.notes=move_attribute(dic,{'note','notes'},strict=False)
 		if self.type is 'exercise':
 			self.name = move_attribute(dic, {'name'},strict=False)
@@ -109,39 +110,30 @@ class Node:
 		return self.__dict__
 
 	def __str__(self):
+		msg=""
+		if self.intuitions:
+			for intuition in self.intuitions:
+				msg = msg + intuition + "\n"
+		if self.examples:
+			for example in self.examples:
+				msg = msg + example + "\n"
+		if self.counterexamples:
+			for counter in self.counterexamples:
+				msg=msg+counter+"\n"
+		if self.notes:
+			for single_note in self.notes:
+				msg=msg+ single_note+"\n"
+
 		if self.type=="definition":
-			msg = "(%s,%s,%s,%s,%d)\n" % (self.name, self.plural, self.type, self.description, self.weight)
-			if self.intuitions:
-				for intuition in self.intuitions:
-					msg = msg + intuition + "\n"
-			if self.examples:
-				for example in self.examples:
-					msg = msg + example + "\n"
-			if self.counterexamples:
-				for counter in self.counterexamples:
-					msg=msg+counter+"\n"
-			if self.notes:
-				for single_note in self.notes:
-					msg=msg+ single_note+"\n"
+			first_part = "(%s,%s,%s,%s,%d)\n" % (self.name, self.plural, self.type, self.description, self.weight)
+
 		else:
-			msg = "(%s,%s,%s,%d)\n" % (self.name,self.type,self.description,self.weight)
-			if self.intuitions:
-				for intuition in self.intuitions:
-					msg = msg + intuition + "\n"
-			if self.examples:
-				for example in self.examples:
-					msg = msg + example + "\n"
-			if self.counterexamples:
-				for counter in self.counterexamples:
-					msg=msg+counter+"\n"
-			if self.notes:
-				for single_note in self.notes:
-					msg=msg+ single_note+"\n"
+			first_part = "(%s,%s,%s,%d)\n" % (self.name,self.type,self.description,self.weight)
 			if self.proofs:
 				for single_proof in self.proofs:
 					for x in single_proof:
 						msg=msg+str(x)+": "+single_proof[x]+"\n"
-		return msg
+		return first_part+msg
 
 	def __eq__(self,other):
 		if self.name != other.name:
@@ -171,9 +163,13 @@ class Node:
 		return self._name
 	@name.setter
 	def name(self, new_name):
-		assert is_capitalized(new_name)
-		#assert dunderscore_count(new_name)==0;
-		self._name = check_type_and_clean(new_name, str)
+		if new_name:
+			assert is_capitalized(new_name)
+			assert dunderscore_count(new_name)==0;
+			self._name = check_type_and_clean(new_name, str)	
+		else:
+			self._name= check_type_and_clean(new_name, type(None))
+			
 
 	@property
 	def type(self):
@@ -218,19 +214,31 @@ class Node:
 	@intuitions.setter
 	def intuitions(self, new_intuition):
 		assert is_content_clean(new_intuition)
-		self._intuitions = check_type_and_clean(new_intuition, str, list_of=True)
+		cleaned_intuition = check_type_and_clean(new_intuition, str, list_of=True)
+		for x in cleaned_intuition:
+			assert dunderscore_count(x)==0
+		self._intuitions=cleaned_intuition
+
 	def append_intuition(self, add_intuition):
+		assert is_content_clean(add_intuition)
+		assert dunderscore_count(add_intuition)==0
 		self._intuition.append(add_intuition)
 
 	@property
 	def examples(self):
 		return self._examples
+
 	@examples.setter
 	def examples(self, new_examples):
 		assert is_content_clean(new_examples)
-		self._examples = check_type_and_clean(new_examples, str, list_of=True)
-	
+		cleaned_examples = check_type_and_clean(new_examples, str, list_of=True)
+		for x in cleaned_examples:
+			assert dunderscore_count(x)==0
+		self._examples=cleaned_examples
+
 	def append_example(self, add_example):
+		assert is_content_clean(add_example)
+		assert dunderscore_count(add_example)==0
 		self._examples.append(add_example)
 
 	@property
@@ -239,7 +247,16 @@ class Node:
 	@counterexamples.setter
 	def counterexamples(self, new_examples):
 		assert is_content_clean(new_examples)
-		self._counterexamples = check_type_and_clean(new_examples, str, list_of=True)
+		cleaned_counterexamples = check_type_and_clean(new_examples, str, list_of=True)
+		for x in cleaned_counterexamples:
+			assert dunderscore_count(x)==0
+		self._counterexamples=cleaned_counterexamples
+
+	def append_counterexample(self, add_counterexample):
+		assert is_content_clean(add_counterexample)
+		assert dunderscore_count(add_counterexample)==0
+		self._examples.append(add_counterexample)
+
 
 	@property
 	def notes(self):
@@ -247,9 +264,14 @@ class Node:
 	@notes.setter
 	def notes(self, new_notes):
 		assert is_content_clean(new_notes)
-		self._notes = check_type_and_clean(new_notes, str, list_of=True)
+		cleaned_notes = check_type_and_clean(new_notes, str, list_of=True)
+		for x in cleaned_notes:
+			assert dunderscore_count(x)
+		self._notes=cleaned_notes
 
 	def append_note(self, add_note):
+		assert is_content_clean(add_note)
+		assert dunderscore_count(add_note)==0
 		self._notes.append(add_note)
 
 
@@ -281,8 +303,19 @@ class Theorem(Node):
 		self.type = "theorem"
 		super(Theorem, self,).__init__(dic)
 		self.proofs = move_attribute(dic, {'proofs', 'proof'}, strict=False)
-		assert self.weight>=3
-	
+        
+
+	#Override the weight setter for specific Theorem weight
+	@property
+	def weight(self):
+		return self._weight
+
+	@weight.setter
+	def weight(self, new_weight):
+		clean_weight = check_type_and_clean(new_weight, int) # but in the future we will accept decimals (floats) too!
+		assert clean_weight >= 3 and clean_weight <= 10
+		self._weight = clean_weight
+
 	@property
 	def proofs(self):
 		return self._proofs
@@ -300,4 +333,28 @@ class Exercise(Node):
 	def __init__(self, dic):
 		self.type = "exercise"
 		super(Exercise, self).__init__(dic)
-		assert self.weight<=3
+		self.proofs = move_attribute(dic, {'proofs', 'proof'}, strict=False)
+		
+	@property
+	def weight(self):
+		return self._weight
+
+	@weight.setter
+	def weight(self, new_weight):
+		clean_weight = check_type_and_clean(new_weight, int)
+		assert clean_weight >= 1 and clean_weight <= 3
+		self._weight = clean_weight
+
+
+	@property
+	def proofs(self):
+	       	return self._proofs
+   	
+	@proofs.setter
+	def proofs(self,new_proofs):
+	       	assert is_content_clean(new_proofs)
+	       	self._proofs = check_type_and_clean(new_proofs, dict, list_of=True)
+	def append_proof(self, add_proof): # these need type checking too
+		self._proofs.append(add_proof)
+
+
--- server/lib/test_node.py
+++ server/lib/test_node.py
@@ -4,7 +4,9 @@ from lib.node import Theorem
 from lib.node import Exercise
 from lib.node import Definition
 
-# Leave out the type since we plan to check it on the outside
+
+##########################INIT, SETTERS/GETTERS, ATTRIBUTE TESTS################
+
 def test_theorem_copy():
      sample_theorem = {
      	"name": "Pythagorean theorem",
@@ -20,7 +22,7 @@ def test_theorem_copy():
      pass
 
 
-def test_theorem():
+def test_theorem_setter_getter():
     pre_node = {"name": "Pythagorean theorem", "weight": 6, "description": "When the leg is a and the leg is b and the hypotenuse is c, then a^2+b^2=c^2.", "intuition": "A simple explanation.", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
     node = Theorem(pre_node)
     print(node)
@@ -28,24 +30,51 @@ def test_theorem():
     assert node.type=="theorem"
     assert node.weight==6
     assert node.description=="When the leg is a and the leg is b and the hypotenuse is c, then a^2+b^2=c^2."
+    assert node.intuitions==["A simple explanation."]
     assert node.examples== ["Example 1 is now long enough.", "Example 2 is now long."]
     assert node.proofs== [{"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}]
+
+def test_theorem_bad_attributes():
+    pre_node = {"name": "Pythagorean theorem", "weight": 6, "description": "When the leg is a and the leg is b and the hypotenuse is c, then a^2+b^2=c^2.", "intuition": "A simple explanation.", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
+    node = Theorem(pre_node)
     with pytest.raises(AttributeError) as e:
         node.intuition=="A simple explanation"
     with pytest.raises(AttributeError) as e:
         node.plural==""
 
+def test_exercise_setter_getter():
+      pre_node = { "weight": 2, "description": "Three Four Five Triangle", "intuition": "3^2+4^2=5^2", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
+      node=Exercise(pre_node)
+      print(node)
+      assert node.name==None
+      assert node.type=="exercise"
+      assert node.weight==2
+      assert node.description=="Three Four Five Triangle"
 
-def test_definition():
-    sample_definition = {"name": "__Triangle__", "weight": 1, "description": "A __triangle__ is a 3 sided polygon.", "intuition": "A simple explanation", "examples": ["Long Long Long Example 1", "text text text Example 2"]}
+
+
+def test_exercise_bad_attributes():
+          pre_node = { "weight": 2, "description": "Three Four Five Triangle", "intuition": "3^2+4^2=5^2", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
+          node=Exercise(pre_node)
+          with pytest.raises(AttributeError) as e:
+              node.plural==""
+          
+          
+def test_definition_setters_getters():
+    sample_definition = {"name": "Triangle", "weight": 1, "description": "A __triangle__ is a 3 sided polygon.", "intuition": "A simple explanation", "examples": ["Long Long Long Example 1", "text text text Example 2"]}
     node=Definition(sample_definition)
     print(node)
-    assert node.name=="__Triangle__"
+    assert node.name=="Triangle"
     assert node.type=="definition"
     assert node.weight==1
     assert node.description=="A __triangle__ is a 3 sided polygon."
     #assert node.plural=="__Triangles__"
     assert node.examples== ["Long Long Long Example 1", "text text text Example 2"]
+
+
+def test_definition_bad_attributes():
+    sample_definition = {"name": "Triangle", "weight": 1, "description": "A __triangle__ is a 3 sided polygon.", "intuition": "A simple explanation", "examples": ["Long Long Long Example 1", "text text text Example 2"]}
+    node=Definition(sample_definition)
     with pytest.raises(AttributeError) as e:
         node.intuition=="A simple explanation"
     with pytest.raises(AttributeError) as e:
@@ -53,11 +82,52 @@ def test_definition():
     with pytest.raises(ValueError) as e:
         node.type="Something Weird"
 
+############################ DUNDERSCORE, WEIGHT TESTS ##########################################
 
+def test_definition_dunderscore_exists_content():
+    sample_definition = {"name": "Triangle", "weight": 1, "description": "A __triangle__ is a 3 sided polygon.", "intuition": "A simple explanation", "examples": ["Long Long Long Example 1", "text text text Example 2"]}
+    node=Definition(sample_definition)
+    with pytest.raises(AssertionError) as e:
+        node.description="A triangle is a 3 sided polygon."
+    with pytest.raises(AssertionError) as e:
+        node.name="__DunderScore__"
 
+def test_theorem_no_dunderscore():
+    pre_node = {"name": "Pythagorean theorem", "weight": 6, "description": "When the leg is a and the leg is b and the hypotenuse is c, then a^2+b^2=c^2.", "intuition": "A simple explanation.", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
+    node = Theorem(pre_node)
+    with pytest.raises(AssertionError) as e:
+        node.description="A __triangle__ is a 3 sided polygon."
+    with pytest.raises(AssertionError) as e:
+        node.name="__DunderScore__"
 
+def test_exercise_no_dunderscore():
+    pre_node={ "weight": 2, "description": "Three Four Five Triangle", "intuition": "3^2+4^2=5^2", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
+    node=Exercise(pre_node)
+    with pytest.raises(AssertionError) as e:
+        node.description="A __triangle__ is a 3 sided polygon."
+    with pytest.raises(AssertionError) as e:
+        node.examples=["__DunderScore__ is in here", "In here as __well__", "Will this pass?"]
 
+def test_definition_weights():
+    sample_definition = {"name": "Triangle", "weight": 1, "description": "A __triangle__ is a 3 sided polygon.", "intuition": "A simple explanation", "examples": ["Long Long Long Example 1", "text text text Example 2"]}
+    node=Definition(sample_definition)
+    with pytest.raises(AssertionError) as e:
+        node.weight=100
 
+def test_theorem_weights():
+     pre_node = {"name": "Pythagorean theorem", "weight": 6, "description": "When the leg is a and the leg is b and the hypotenuse is c, then a^2+b^2=c^2.", "intuition": "A simple explanation.", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
+     node = Theorem(pre_node)
+     with pytest.raises(AssertionError) as e:
+         node.weight=2
+    
+
+def test_exercise_weights():
+    pre_node={ "weight": 2, "description": "Three Four Five Triangle", "intuition": "3^2+4^2=5^2", "examples": ["Example 1 is now long enough.", "Example 2 is now long."],"proof": {"type": "fake", "content": "Left side: You have $n$ people.  You choose $k$ of them to be in a committee, and from the committee, you choose $1$ to be the chairperson.  Right side: You have $n$ people.  You choose $1$ of them to be the chairperson.  From the remaining $n-1$ of them, you choose $k-1$ of them to complete the committee."}}
+    node=Exercise(pre_node)
+    with pytest.raises(AssertionError) as e:
+         node.weight=6
+    
+    
 
 
 
