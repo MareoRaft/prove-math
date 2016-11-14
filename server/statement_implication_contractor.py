2ed914ae11eda5e0802af58ec3c6fe38b54a25df
Matthew Lancellotti <mareoraft@gmail.com>
2016-11-13 21:16:02 -0500

    added more cycle-related functions. these should be migrated into appropriate places once things work nicely.

--- server/statement_implication_contractor.py
+++ server/statement_implication_contractor.py
@@ -2,12 +2,6 @@ import networkx as nx
 from lib.networkx.classes import digraph_extend
 from lib.networkx.classes import dag
 
-# SEE simple_cycles AND cycle_basis
-
-def partition_cycle_clusters(cycle_nodes):
-	"Given a bunch of nodes (that are ALL part of some cycle), return a set where each element is a single cycle"
-
-
 def remove_sources_repeatedly(DG):
 	while True:
 		sources = DG.sources()
@@ -17,24 +11,54 @@ def remove_sources_repeatedly(DG):
 		else:
 			return DG
 
+def remove_sinks_repeatedly(DG):
+	while True:
+		sinks = DG.sinks()
+		if sinks:
+			DG.remove_nodes_from(sinks)
+		else:
+			return DG
+
+# SEE simple_cycles AND cycle_basis AND find_cycle for three useful BUILTIN methods!
+
+def is_cycle_edge(DG, edge):
+	ancestor_nodes = DG.ancestors(edge[0])
+	return edge[1] in ancestor_nodes
+
+def is_cycle_cluster_graph(DG):
+	if DG.sources():
+		return False
+	if DG.sinks():
+		return False
+	for edge in DG.edges():
+		if not is_cycle_edge(DG, edge):
+			return False
+	return True
+
+def partition_cycle_clusters(CG):
+	"Given a graph that consists of cycle clusters only, return a set where each element is a single cycle cluster"
+	if not is_cycle_cluster_graph(CG):
+		raise ValueError('The input graph cannot have any sources, sinks, or any edges that are not part of a cycle.')
+	cycle_clusters = set()
+	while CG.nodes():
+		node = CG.nodes()[0]
+		cycle_cluster = CG.ancestors(node)
+		cycle_cluster.add(node) # don't forget node!
+		CG.remove_nodes_from(cycle_cluster)
+		cycle_clusters.add(frozenset(cycle_cluster))
+	return cycle_clusters
 
 def find_dir_cycles(G):
 	"Look for something like P0 --> P1 --> P2 --> P0 and return the guilty :) nodes"
-
-
-
 	GC = G.clone()
 	# delete all sources, since they can't be part of a cycle
+	remove_sources_repeatedly(GC)
 	# delete all sinks, for the same reason
-	# then same for sinks
-
-
-	# repeat as long as you can!
-
+	remove_sinks_repeatedly(GC)
 
 	# what you are left with is cycle clusters
-	set_of_cycle_clusters = partition_cycle_clusters(cycle_nodes)
-	return set_of_cycle_clusters
+	cycle_clusters = partition_cycle_clusters(GC, cycle_nodes)
+	return cycle_clusters
 
 def contract_dir_cycle():
 	# or maybe just "contract nodes" can work in full generality
--- server/test_statement_implication_contractor.py
+++ server/test_statement_implication_contractor.py
@@ -34,8 +34,85 @@ def test_remove_sources_repeatedly():
 	assert DG.sources() == set()
 	assert len(DG.nodes()) == 5
 
+def test_remove_sinks_repeatedly():
+	DG = nx.DiGraph()
+	DG.add_path(['a', 'b', 'c'])
+	remove_sinks_repeatedly(DG)
+	assert not DG.nodes()
 
+	DG = nx.DiGraph()
+	DG.add_path(['a', 'b', 'c'])
+	DG.add_path(['a', 'd', 'c'])
+	remove_sinks_repeatedly(DG)
+	assert not DG.nodes()
 
+	DG = nx.DiGraph()
+	DG.add_cycle(['a', 'b', 'c'])
+	remove_sinks_repeatedly(DG)
+	assert len(DG.nodes()) == 3
+
+	DG = nx.DiGraph()
+	DG.add_edge('s', 'a')
+	DG.add_cycle(['a', 'b', 'c', 'd'])
+	DG.add_edge('d', 't')
+	remove_sinks_repeatedly(DG)
+	assert DG.sinks() == set()
+	assert len(DG.nodes()) == 5
+
+def test_is_cycle_edge():
+	DG = nx.DiGraph()
+	DG.add_path(['a', 'b', 'c'])
+	assert not is_cycle_edge(DG, ('a', 'b'))
+
+	DG = nx.DiGraph()
+	DG.add_cycle(['a', 'b', 'c'])
+	assert is_cycle_edge(DG, ('a', 'b'))
+
+	DG = nx.DiGraph()
+	DG.add_path(['a', 'b', 'c', 'a', 'e', 'f', 'g', 'h', 'f'])
+	assert is_cycle_edge(DG, ('a', 'b'))
+	assert not is_cycle_edge(DG, ('e', 'f'))
+	assert is_cycle_edge(DG, ('f', 'g'))
+
+def test_is_cycle_cluster_graph():
+	DG = nx.DiGraph()
+	DG.add_cycle(['a', 'b', 'c'])
+	assert is_cycle_cluster_graph(DG)
+
+	DG = nx.DiGraph()
+	DG.add_path(['a', 'b', 'c'])
+	assert not is_cycle_cluster_graph(DG)
+
+	DG = nx.DiGraph()
+	DG.add_cycle(['a'])
+	DG.add_edge('a', 'b')
+	DG.add_cycle(['b'])
+	assert not is_cycle_cluster_graph(DG)
+
+	DG = nx.DiGraph()
+	DG.add_path(['a', 'b', 'c', 'a', 'e', 'f', 'g', 'h', 'f'])
+	assert not is_cycle_cluster_graph(DG)
+
+def test_partition_cycle_clusters():
+	CG = nx.DiGraph()
+	assert partition_cycle_clusters(CG) == set()
+
+	CG = nx.DiGraph()
+	CG.add_cycle(['a', 'b', 'c'])
+	assert partition_cycle_clusters(CG) == {frozenset({'a', 'b', 'c'})}
+
+	CG = nx.DiGraph()
+	CG.add_cycle(['a', 'b', 'c', 'd'])
+	CG.add_cycle(['a', 'b', 't', 'm'])
+	assert partition_cycle_clusters(CG) == {frozenset({'a', 'b', 'c', 'd', 't', 'm'})}
+
+	DG = nx.DiGraph()
+	DG.add_path(['a', 'b', 'c', 'a', 't', 'a'])
+	DG.add_path(['x', 'y', 'z', 'x'])
+	assert partition_cycle_clusters(DG) == {
+		frozenset({'a', 'b', 'c', 't'}),
+		frozenset({'x', 'y', 'z'}),
+	}
 
 
 
